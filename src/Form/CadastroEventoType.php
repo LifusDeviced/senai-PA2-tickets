5ac0afb808830870da3e544fc15ccfd3dd447ff4
Fernando Manechini <fmanechini@gmail.com>
2019-10-14 17:22:51 -0300

    Criado formulario de cadastro do evento

--- src/Form/CadastroEventoType.php
+++ src/Form/CadastroEventoType.php
@@ -2,21 +2,29 @@
 
 namespace App\Form;
 
+use App\Entity\Evento;
 use App\Entity\LocalEvento;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class LocalEventoType extends AbstractType
+class CadastroEventoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nome_local', TextType::class, ['label' => 'Nome do Local'])
-            ->add('capacidade', IntegerType::class, ['label' => 'Capacidade'])
+            ->add('nome_evento', TextType::class, ['label' => 'Nome do Evento'])
+            ->add('data', DateTimeType::class, ['label' => 'Data do Evento'])
+            ->add('valor', IntegerType::class, ['label' => 'Valor'])
+            ->add('id_local_evento', EntityType::class,[
+                'class' => LocalEvento::class,
+                'choice_label' => 'nome_local'
+            ])
             ->add('confirma', SubmitType::class)
         ;
     }
@@ -24,7 +32,7 @@ class LocalEventoType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => LocalEvento::class,
+            'data_class' => Evento::class,
         ]);
     }
 }
