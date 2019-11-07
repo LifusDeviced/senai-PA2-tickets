b80785bcbef1e8133521d25316370e94e6f50f58
Fernando Manechini <fmanechini@gmail.com>
2019-11-07 14:19:38 -0300

    Feito backend dos detalhes do evento, puxando os dados diretamente do banco

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Evento;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailsController extends AbstractController
@@ -10,10 +12,15 @@ class DetailsController extends AbstractController
     /**
      * @Route("/details", name="details")
      */
-    public function index()
+    public function details(Request $request)
     {
+        $id =$request->get('eventos');
+        if($id != null) {
+            $evento = $this->getDoctrine()->getRepository(Evento::class)->find($id);
+        }
+
         return $this->render('details/index.html.twig', [
-            'controller_name' => 'DetailsController',
+            'evento' => $evento,
         ]);
     }
 }
